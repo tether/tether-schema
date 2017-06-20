3821aee0cb06f25b53e393bcbddb61e133cfe0db
bredele <olivier.wietrich@gmail.com>
2017-06-20 15:11:25 -0600

    Trigger TypeError if wrong type

--- test/tether-schema.js
+++ test/tether-schema.js
@@ -21,3 +21,22 @@ test('should trigger ReferenceError if field does not exist', assert => {
     assert.equal(e.message, 'field email is not defined')
   }
 })
+
+
+test('should trigger TypeError if field exist but wrong type', assert => {
+  assert.plan(2)
+  const schema = protocol(`
+    message User {
+      required string email = 1;
+    }
+  `)
+
+  try {
+    schema('User', {
+      email: true
+    })
+  } catch (e) {
+    assert.equal(e.name, 'TypeError')
+    assert.equal(e.message, 'field email is not a string')
+  }
+})
